90e3ff76bd4f520bbdae9617448d76a8ff4686a1
tomwoj6 <tomwoj6@gmail.com>
2016-02-15 20:48:49 +0100

    poprawki

--- src/FindbulbBundle/Controller/FindbulbController.php
+++ src/FindbulbBundle/Controller/FindbulbController.php
@@ -10,10 +10,12 @@ class FindbulbController extends Controller
         
         $em = $this->getDoctrine()->getManager();
         $ideas = $em->getRepository('FindbulbBundle:Idea')->findAll();
+        $categories = $em->getRepository('FindbulbBundle:Category')->findAll();
         
-        return $this->render('FindbulbBundle:Home:index.html.twig', array(
-            'ideas' => $ideas
-        ));
+        return $this->render('FindbulbBundle:Home:index.html.twig', [
+            'ideas' => $ideas,
+            'categories' => $categories
+        ]);
     }
 
 }
